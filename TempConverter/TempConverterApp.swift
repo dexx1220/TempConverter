23667b6695d0606f9ff65df66add7a6ace19e83e
Dexter Heng <dexter.heng@hqo.co>
2022-06-04 15:42:12 -0400

    Initial Commit

---
+++ TempConverter/TempConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempConverterApp.swift
+//  TempConverter
+//
+//  Created by Dexter Heng on 6/4/22.
+//
+
+import SwiftUI
+
+@main
+struct TempConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
