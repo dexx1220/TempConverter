e9ac721d90af4ef4b32533de512d464c35399d1e
Dexter Heng <dexter.heng@hqo.co>
2022-06-04 16:08:40 -0400

    First commit

--- TempConverter/ContentView.swift
+++ TempConverter/ContentView.swift
@@ -8,9 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var celsius = 0
+    @State private var fahrenheit = 0
+    
+    var convertedToFahrenheit: Double {
+        return (Double(celsius) * 9/5) + Double(32)
+    }
+    
+    var convertedToCelsius: Double {
+        return Double(fahrenheit - 32) * 5/9
+    }
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            HStack {
+                VStack {
+                    Text("Enter Celsius:")
+                    TextField("Celsius", value: $celsius, formatter: NumberFormatter())
+                        .multilineTextAlignment(TextAlignment.center)
+                }
+                VStack {
+                    Text("In Fahrenheit: \(convertedToFahrenheit, specifier: "%.2f")")
+                }
+            }
             .padding()
+            
+            HStack {
+                VStack {
+                    Text("Enter Fahrenheit:")
+                    TextField("Fahrenheit", value: $fahrenheit, formatter: NumberFormatter())
+                        .multilineTextAlignment(TextAlignment.center)
+                }
+                VStack {
+                    Text("In Celsius: \(convertedToCelsius, specifier: "%.2f")")
+                }
+            }
+            .padding()
+        }
     }
 }
 
